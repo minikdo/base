975ec39ab2999c02e056f6060087f24fbc0ef03b
Dominik Szmek <do@minik.pl>
2019-05-11 12:42:10 +0200

    daily commit

--- .emacs.d/custom-functions.el
+++ .emacs.d/custom-functions.el
@@ -87,3 +87,14 @@
       vc-root-dir))) ;; default to vc root if sentry not given
 
 
+
+;; Dired
+
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map "F" 'my-dired-find-file)
+     (defun my-dired-find-file (&optional arg)
+              "Open each of the marked files, or the file under the point, or when prefix arg, the next N files "
+              (interactive "P")
+              (let* ((fn-list (dired-get-marked-files nil arg)))
+                         (mapc 'find-file fn-list)))))
