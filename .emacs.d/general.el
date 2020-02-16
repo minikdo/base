6e8e90f1907a3032bb9a614153845dfb30292ef7
Dominik Szmek <do@minik.pl>
2020-02-16 11:53:46 +0100

    daily commit

--- .emacs.d/general.el
+++ .emacs.d/general.el
@@ -8,6 +8,9 @@
 ;; (add-to-list 'default-frame-alist
              ;; '(font . "Hack-12"))
 
+(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+
 (add-to-list 'package-archives
              '("melpa" . "https://stable.melpa.org/packages/") t)
 
