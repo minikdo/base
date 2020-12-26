38389f507a611c32a5de6703bf7569193c24a09a
Dominik Szmek <do@minik.pl>
2020-12-26 12:04:04 +0100

    Add custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ed91d4e59412defda16b551eb705213773531f30eb95b69319ecd142fab118ca" default)))
+ '(fringe-mode 10 nil (fringe))
+ '(linum-format " %6d ")
+ '(package-selected-packages
+   (quote
+    (multiple-cursors centaur-tabs rainbow-mode persistent-scratch python python-mode python-environment epc emmet-mode which-key web-mode tabbar smex session pyvenv projectile pod-mode php-mode muttrc-mode mutt-alias markdown-mode jedi initsplit htmlize graphviz-dot-mode git-annex folding flx-ido eproject diminish csv-mode clues-theme browse-kill-ring boxquote bm bar-cursor apache-mode))))
