0b805fac03a2fdf580e62398bc94fc348340a4fd
Dominik Szmek <do@minik.pl>
2024-02-28 11:21:16 +0100

    Update custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,6 +7,7 @@
    ["#E8E8E8" "#3C3C3C" "#616161" "#0E0E0E" "#252525" "#3C3C3C" "#171717" "#0E0E0E"])
  '(custom-safe-themes
    '("dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "439d4ce8295685fc36fc119a062d0283bb05436ae841b053af76e9a5e42bc670" "4ce515d79ef94f3b7651c8e2a7c7d81814b9ca911eb2a16955f45f4555faf524" "18c5ec0e4d1723dbeadb65d17112f077529fd24261cb8cd4ceee145e6a6f4cd1" "ed91d4e59412defda16b551eb705213773531f30eb95b69319ecd142fab118ca" default))
+ '(fci-rule-color "#F1F1F1")
  '(fringe-mode 10 nil (fringe))
  '(linum-format " %6d ")
  '(main-line-color1 "#222232")
@@ -14,6 +15,8 @@
  '(org-agenda-files
    '("/home/domino/.agenda/estates.org" "/home/domino/.agenda/agenda.org" "/home/domino/.agenda/issues.org" "/home/domino/.agenda/links.org" "/home/domino/.agenda/notes.org"))
  '(package-hidden-regexps '("\\`backtrace\\'" ""))
+ '(package-selected-packages
+   '(emmet-mode yasnippet-snippets yaml-mode which-key web-mode virtualenvwrapper use-package smex rainbow-mode projectile popup paredit org-bullets modus-themes lsp-mode jinja2-mode haskell-mode git-annex flx-ido dockerfile-mode diminish corfu clues-theme apache-mode tao-theme smartparens rust-mode nftables-mode multiple-cursors jsonrpc jedi default-text-scale))
  '(powerline-color1 "#222232")
  '(powerline-color2 "#333343")
  '(safe-local-variable-values
@@ -48,4 +51,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(hl-line ((t (:background "gray21"))))
- '(isearch-fail ((t (:background "red")))))
+ '(isearch-fail ((t (:background "red"))))
+)
