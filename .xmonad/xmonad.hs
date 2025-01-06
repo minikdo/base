4588338748230823d4ac204f008e4d64ab2d6f19
Dominik Szmek <do@minik.pl>
2025-01-06 12:33:51 +0100

    Update ManageHook in xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -162,7 +162,7 @@ myManageHook = composeAll
       , className =? "mpv"                --> doFullFloat
       , className =? "Arandr"             --> doCenterFloat
       -- , className =? "Evince"             --> doRectFloat (W.RationalRect 0.2 0.05 0.6 0.9)
-      , className =? "Pavucontrol"        --> doRectFloat (W.RationalRect 0.15 0.15 0.7 0.7)
+      , className =? "pavucontrol"        --> doRectFloat (W.RationalRect 0.15 0.15 0.7 0.7)
       , stringProperty "WM_WINDOW_ROLE" =? "pop-up"               --> doFloat
       , stringProperty "WM_WINDOW_ROLE" =? "gimp-message-dialog"  --> doFloat
       , isDialog                                                  --> doFloat
